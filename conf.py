86ddb23fd33d00a2011e49c3e25d3e465c4dddb0
redteampanda-ng <31235211+redteampanda-ng@users.noreply.github.com>
2024-01-16 09:02:29 +0100

    chore: prepare documentation for new version

--- conf.py
+++ conf.py
@@ -1,79 +1,30 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-# -- Project information -----------------------------------------------------
-
-project = 'ASGARD Management Center Manual'
-copyright = '2022, Nextron Systems'
-author = 'Stephan Kaiser, Florian Roth, Christian Burkard'
-
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+project = 'ASGARD Management Center v3 Manual'
+version="3.0"
+copyright = '2023, Nextron Systems GmbH'
+author = 'Nextron Systems'
 extensions = [
     'sphinx.ext.autosectionlabel',
     'sphinx_rtd_theme'
 ]
-
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
+source_suffix = '.rst'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv/*']
+master_doc = 'index'
+language = "en"
 html_theme = 'sphinx_rtd_theme'
-
 html_theme_options = {
     'prev_next_buttons_location': 'both',
     'style_external_links': True
 }
-
 html_logo = "images/html/asgard-logo.png"
 html_favicon = "images/html/favicon.ico"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-# These paths are either relative to html_static_path
-# or fully qualified paths (eg. https://...)
-html_css_files = [
-    'css/custom.css',
-]
-
-# Master
-master_doc = 'index'
-
-# Enable -- to -
+html_css_files = ['css/custom.css',]
+epub_title = project
+epub_exclude_files = ['search.html']
+intersphinx_mapping = {'https://docs.python.org/': None}
 smartquotes = False
-
-# Use :ref: for subsections
 autosectionlabel_prefix_document = True
 autosectionlabel_maxdepth = 5
-
+# disable epub mimetype warnings
+suppress_warnings = ["epub.unknown_project_files"]
