d649abd5a7420dddc8fb1db03c8cc93e80eb5903
redteampanda-ng <31235211+redteampanda-ng@users.noreply.github.com>
2024-07-18 08:12:53 +0200

    chore: add changes for new rtd build process

--- conf.py
+++ conf.py
@@ -1,3 +1,5 @@
+import os
+
 project = 'ASGARD Management Center v3 Manual'
 version="3.0"
 copyright = '2023, Nextron Systems GmbH'
@@ -20,6 +22,12 @@ html_logo = "images/html/asgard-logo.png"
 html_favicon = "images/html/favicon.ico"
 html_static_path = ['_static']
 html_css_files = ['css/custom.css',]
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
+
 epub_title = project
 epub_exclude_files = ['search.html']
 intersphinx_mapping = {'https://docs.python.org/': None}
